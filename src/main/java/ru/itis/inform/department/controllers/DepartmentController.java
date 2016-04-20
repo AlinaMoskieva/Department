4153d5d2e3e018265e34084d2a4317c8b945ef12
Alina Moskieva <alinamoskieva@gmail.com>
2016-04-20 15:27:53 +0300

    Controller amended

--- src/main/java/ru/itis/inform/department/controllers/DepartmentController.java
+++ src/main/java/ru/itis/inform/department/controllers/DepartmentController.java
@@ -2,6 +2,9 @@ package ru.itis.inform.department.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import ru.itis.inform.department.controllers.dto.DocumentDto;
+import ru.itis.inform.department.controllers.dto.ParticipantDto;
+import ru.itis.inform.department.controllers.dto.ParticipantsDto;
 import ru.itis.inform.department.dao.models.Document;
 import ru.itis.inform.department.dao.models.Participants;
 import ru.itis.inform.department.dao.models.User;
@@ -22,13 +25,17 @@ public class DepartmentController {
     private UsersService usersService;
 
     @RequestMapping(value = "/document/{id}", method = RequestMethod.GET)
-    public Document getDocumentById(@PathVariable int id){
-        return documentService.getDocumentsInformation(id);
+    public DocumentDto getDocumentById(@PathVariable int id){
+        DocumentDto dto = new DocumentDto(documentService.getDocumentsInformation(id));
+        return dto;
     }
 
     @RequestMapping(value = "/document/{documentId}/additional", method = RequestMethod.GET)
-    public List<Participants> getAdditionalToDocument(@PathVariable int documentId){
-        return participantService.getListOfPArticipants(documentId);
+    public List<ParticipantDto> getAdditionalToDocument(@PathVariable int documentId){
+        ParticipantsDto dto = new ParticipantsDto();
+        dto.setListParticipants(participantService.getListOfPArticipants(documentId));
+
+        return dto.getParticipants();
     }
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String getHello(){
